8d92b25960fc9c3e5bb38d8714038d53ba91eb68
retkiewi <krzret@student.agh.edu.pl>
2022-11-01 16:43:19 +0100

    add test chamber, debug

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import argparse
 from mpi4py import MPI
 import numpy as np
 
-def calulate_value(V_prev: tuple[int, int, int, int]):
+
+def calculate_value(V_prev: tuple[int, int, int, int]):
     return sum(V_prev)/4
 
 
@@ -24,17 +25,18 @@ def add_boundary_condition(arr: np.ndarray, gap_size: int, external_voltage: flo
     arr[:, -1] = 0
 
 
-def get_chunk(arr: np.ndarray, rank: int, size: int):
-    grid_size = arr.shape[0]-2
+def get_chunk(grid_size: int, rank: int, size: int) -> tuple[int, int]:
+    height = grid_size//3
 
-    height = grid_size//size
+    length = ceil(grid_size*height/size)
 
-    start = rank*height
-    end = start+height
+    start = length*rank
+    end = start+length
 
-    if rank == size-1: end = grid_size 
+    if rank == size-1:
+        end = grid_size*height
 
-    return arr[start:end+2, :]
+    return start, end
 
 
 def main(**kwargs):
@@ -46,9 +48,9 @@ def main(**kwargs):
 
     grid_size: int = kwargs['grid_size']
 
-    itemsize = MPI.DOUBLE.Get_size() 
-    if rank == 0: 
-        nbytes = (grid_size+2) ** 2 * itemsize 
+    itemsize = MPI.DOUBLE.Get_size()
+    if rank == 0:
+        nbytes = (grid_size+2) ** 2 * itemsize
     else:
         nbytes = 0
 
@@ -57,44 +59,58 @@ def main(**kwargs):
     # create a numpy array whose data points to the shared mem
     buf, itemsize = win.Shared_query(0)
 
-    prev_layer = np.ndarray(buffer=buf, dtype='d', shape=(grid_size+2, grid_size+2))
-    add_boundary_condition(prev_layer, kwargs['gap_size'], kwargs['external_voltage'])
+    prev_layer = np.ndarray(buffer=buf, dtype='d',
+                            shape=(grid_size+2, grid_size+2))
+    add_boundary_condition(
+        prev_layer, kwargs['gap_size'], kwargs['external_voltage'])
 
-    curr_layer = np.ndarray(buffer=buf, dtype='d', offset=nbytes, shape=(grid_size+2, grid_size+2))
-    add_boundary_condition(curr_layer, kwargs['gap_size'], kwargs['external_voltage'])
+    curr_layer = np.ndarray(buffer=buf, dtype='d',
+                            offset=nbytes, shape=(grid_size+2, grid_size+2))
+    add_boundary_condition(
+        curr_layer, kwargs['gap_size'], kwargs['external_voltage'])
 
     should_stop = False
 
-    while(should_stop):
+    # grid_size = 18, gap = 6
+    gap_start = (grid_size+2-kwargs['gap_size'])//2
+    gap_end = grid_size+2-gap_start
+
+    while (should_stop):
         prev_layer = np.copy(curr_layer)
 
-        prev_chunk = get_chunk(prev_layer, rank, size)
-        curr_chunk = get_chunk(curr_layer, rank, size)
+        chunk_start, chunk_end = get_chunk(grid_size, rank, size)
 
-        for i in range(1, curr_chunk.shape[0]):
-            for j in range(1, curr_chunk.shape[1]):
-                curr_chunk[i, j] = calulate_value((prev_chunk[i-1, j], prev_chunk[i+1, j], prev_chunk[i, j-1], prev_chunk[i, j+1]))
+        for i in range(chunk_start, chunk_end):
+            for j in range(1, 4):
+                x = 1+i % grid_size
+                y = j+3*(i//grid_size)
+                if x in range(gap_start, gap_end) and y in range(gap_start, gap_end):
+                    continue
+                curr_layer[x, y] = calculate_value(
+                    (prev_layer[x-1, y], prev_layer[x+1, y], prev_layer[x, y-1], prev_layer[x, y+1]))
 
         comm.Barrier()
 
         if rank == 0:
-            should_stop = layers_diff(prev_layer=prev_layer, curr_layer=curr_layer) < kwargs['epsilon']
-                
-            for i in range(1,size):
+            should_stop = layers_diff(
+                prev_layer=prev_layer, curr_layer=curr_layer) < kwargs['epsilon']
+
+            for i in range(1, size):
                 comm.send(obj=should_stop, dest=i)
         else:
             should_stop = comm.recv(source=0)
 
         comm.Barrier()
-    
+
     if rank != 0:
         exit(0)
     print(curr_layer)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        prog = 'Program Name',
-        description = 'Description.',
+        prog='Program Name',
+        description='Description.',
     )
 
     parser.add_argument('filename')
